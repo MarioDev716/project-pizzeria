6e696f54c9d9e1fa05be5f5a2a3b45289d8e9430
MarioDev716 <mariusz.sielnicki@gmail.com>
2022-12-11 01:32:11 +0100

    8.5. CartProduct – Aktualizacja sum po zmianie ilości

--- src/js/script.js
+++ src/js/script.js
@@ -397,7 +397,9 @@
 
     announce() {
       const thisWidget = this;
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true,
+      });
       thisWidget.element.dispatchEvent(event);
       // console.log('announce!');
     }
@@ -447,6 +449,9 @@
       const thisCart = this;
       thisCart.dom.toggleTrigger.addEventListener('click', function () {
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
